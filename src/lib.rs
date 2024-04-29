918c842ee4cbbc1d375b9fbfed29c923408d8e16
Mikolaj Szczesny <miszczes0@gmail.com>
2024-04-29 09:46:05 +0200

    geogetter: update docs with examples
    perform pub fixes
    
    Signed-off-by: Mikolaj Szczesny <miszczes0@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -5,31 +5,31 @@
 
 mod url_encoder;
 
-use reqwest::header::USER_AGENT;
-use serde::{Deserialize, Serialize};
+pub use reqwest::header::USER_AGENT;
+pub use serde::{Deserialize, Serialize};
 
 /// Represents the address structure containing administrative, town, county, state, postcode, country, and country code.
 #[derive(Debug, Serialize, Deserialize)]
-struct Address {
-    administrative: String,
-    town: String,
-    county: String,
-    state: String,
-    postcode: String,
-    country: String,
-    country_code: String,
+pub struct Address {
+    pub administrative: String,
+    pub town: String,
+    pub county: String,
+    pub state: String,
+    pub postcode: String,
+    pub country: String,
+    pub country_code: String,
 }
 
 /// Represents the location structure containing latitude, longitude, address type, name, display name, address, and bounding box.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Location {
-    lat: String,
-    lon: String,
-    addresstype: String,
-    name: String,
-    display_name: String,
-    address: Address,
-    boundingbox: Vec<String>,
+    pub lat: String,
+    pub lon: String,
+    pub addresstype: String,
+    pub name: String,
+    pub display_name: String,
+    pub address: Address,
+    pub boundingbox: Vec<String>,
 }
 
 /// Asynchronously retrieves location data from OpenStreetMap based on the input string.
@@ -41,6 +41,32 @@ pub struct Location {
 /// # Returns
 ///
 /// A Result containing a vector of Location structs if successful, or a Box<dyn std::error::Error> if an error occurs.
+///
+/// # Examples
+///
+/// ```
+/// use geogetter::get_location;
+///
+/// #[tokio::main]
+/// async fn main() {
+///     // Input string for location search
+///     let input_str = "Mozzila Headquarters";
+///
+///     // Call the asynchronous function
+///     match get_location(input_str).await {
+///         Ok(locations) => {
+///             // Ensure that the result is not empty
+///             assert!(!locations.is_empty());
+///             // Print the first location for inspection
+///             println!("{:?}", locations[0]);
+///         }
+///         Err(e) => {
+///             // Handle errors
+///             println!("Error occurred: {:?}", e);
+///         }
+///     }
+/// }
+/// ```
 pub async fn get_location(input_str: &str) -> Result<Vec<Location>, Box<dyn std::error::Error>> {
     let url = format!(
         "https://nominatim.openstreetmap.org/search?addressdetails=1&q={}&format=jsonv2&limit=1",
@@ -60,12 +86,12 @@ pub async fn get_location(input_str: &str) -> Result<Vec<Location>, Box<dyn std:
 }
 
 #[cfg(test)]
-mod tests {
+pub mod tests {
     use super::*;
     use tokio::test;
 
     #[test]
-    async fn test_get_location() {
+    pub async fn test_get_location() {
         // Input string for testing
         let input_str = "Lębork";
 
